e3646646d9b423750873e16ec055c7e8146ced32
Кирилл Катранов <katranovrpk@gmail.com>
2025-04-17 22:25:53 +0300

    Remove checkers

--- ParsingVK.py
+++ ParsingVK.py
@@ -1,6 +1,8 @@
 import time
 import vk_api
-from exeptions import ExeptionCheckAnswerKeys
+from frameworks_and_drivers.gateways.parsing_gateway.exeptions import (
+    ExeptionCheckAnswerKeys,
+)
 import logging
 import requests
 
@@ -21,6 +23,7 @@ logger.addHandler(console_handler)
 
 class ParsingVK(BaseGateway):
     def __init__(self):
+        self.post_list = []
         self.ACCESS_TOKEN = (
             "e55cb58be55cb58be55cb58b7be67746d4ee55ce55cb58b828c1488691a1e2af624bda5"
         )
@@ -55,7 +58,7 @@ class ParsingVK(BaseGateway):
             "https://vk.com/topgid_nnov",
         ]
 
-    def create_vk_session(self) -> VkApiMethod:  # type: ignore
+    def create_vk_session(self):  # type: ignore
         vk_session = vk_api.VkApi(token=self.ACCESS_TOKEN)
         return vk_session.get_api()
 
@@ -63,38 +66,6 @@ class ParsingVK(BaseGateway):
         """Приводим из формата https://vk.com/torpedonn -> torpedonn"""
         return [group.strip().rsplit("/", 1)[-1] for group in self.group_url]
 
-    def check_tokens(self) -> None:
-        if not self.ACCESS_TOKEN:
-            message = "Отсутствует обязательная переменная окружения {ACCESS_TOKEN}"
-            logger.critical(message)
-            raise ValueError("Нет переменной окружения SERVICE_KEY")
-
-    def check_api_response_fields(self) -> None:
-        """Проверка полей response на соответствие типу"""
-        for i in range(len(self.response["items"])):
-            if not isinstance(self.response["items"][i]["text"], str):
-                message = "Поле text, пришло не ввиде строки."
-                logger.error(message)
-                raise ValueError(message)
-
-    def check_api_response(self) -> None:
-        """Проверяется валидность ответа API"""
-        if not isinstance(self.response, dict):
-            message = "Response пришёл не ввиде словаря"
-            logger.error(message)
-            raise TypeError("Response пришёл не ввиде словаря")
-
-        if "items" not in self.response:
-            message = "Ключа items нет в response"
-            logger.error(message)
-            raise ExeptionCheckAnswerKeys(message)
-
-        for i in range(len(self.response["items"])):
-            if "text" not in self.response["items"][i]:
-                message = "В ответе нет ключа - text"
-                logger.error(message)
-                raise ExeptionCheckAnswerKeys(message)
-        self.check_api_response_fields()
 
     def parsing(self, amount: int = None) -> None:
         """Парсинг"""
@@ -102,9 +73,9 @@ class ParsingVK(BaseGateway):
         self.amount_posts = amount if amount is not None else 3
         self.list_groups_id = self.loading_group_ids()
         self.list_response = []
-        amount_pars_group = len(self.list_groups_id)
-        if self.amount_posts > amount_pars_group:
-            self.amount_posts_from_single_group = self.amount_posts // amount_pars_group
+        amount_group_for_parsing = len(self.list_groups_id)
+        if self.amount_posts > amount_group_for_parsing:
+            self.amount_posts_from_single_group = self.amount_posts // amount_group_for_parsing
             amount_group_add_1post = self.amount_posts - (
                 len(self.list_groups_id) * self.amount_posts_from_single_group
             )
@@ -118,7 +89,6 @@ class ParsingVK(BaseGateway):
                         count=self.amount_posts_from_single_group,
                     )
                     self.amount_posts_from_single_group -= 1
-                    self.check_api_response()
                     self.list_response.append(self.response)
                     count += 1
                 else:
@@ -127,7 +97,6 @@ class ParsingVK(BaseGateway):
                         v=self.VERSION_VK_API,
                         count=self.amount_posts_from_single_group,
                     )
-                    self.check_api_response()
                     self.list_response.append(self.response)
                     count += 1
         else:
@@ -137,18 +106,16 @@ class ParsingVK(BaseGateway):
                     self.response = vk.wall.get(
                         domain=group, v=self.VERSION_VK_API, count=1
                     )
-                    self.check_api_response()
                     self.list_response.append(self.response)
                 count += 1
 
     def filter_content(self) -> None:
         """
         Приводим спаршенный контент в нужную форму.
-        Пока что форма [{'id': int, 'text': str, orign_photo: bytes}]
-        id - кастомный, просто нумерация
+        Пока что форма [{'id': str, 'text': str, 'image': bytes}]
+        id - hash соответствующего поста
         """
         try:
-            self.post_list = []
             for group_response in self.list_response:
                 for i in range(len(group_response["items"])):
                     if "attachments" not in group_response["items"][i]:
@@ -157,6 +124,7 @@ class ParsingVK(BaseGateway):
                         )
                     post = {}
                     text = group_response["items"][i]["text"]
+                    id_hash = group_response["items"][i]["hash"]
                     if text:
                         post_text = text
                         for attachment in group_response["items"][i]["attachments"]:
@@ -170,15 +138,17 @@ class ParsingVK(BaseGateway):
                                 response = requests.get(post_photo_url)
                                 if response.status_code == 200:
                                     image_bytes = response.content
-                                    print(type(image_bytes))
-                                post["id"] = i
+                                post["id"] = id_hash
                                 post["text"] = post_text
-                                post["image"] = image_bytes
+                                if isinstance(image_bytes, bytes):
+                                    post["image"] = image_bytes
                                 self.post_list.append(post)
         except (ExeptionCheckAnswerKeys, TypeError) as err:
             logger.error(err)
 
     def fetch_content(self) -> list[dict]:
+        self.parsing(amount=100)
+        self.filter_content()
         return self.post_list
 
     def preaty_print(self):
@@ -186,16 +156,4 @@ class ParsingVK(BaseGateway):
         for i in range(len(self.post_list)):
             print("-------------------------------------------------------------")
             print(self.post_list[i])
-            break
-
-
-if __name__ == "__main__":
-    start = time.time()
-    parser_vk = ParsingVK()
-    parser_vk.parsing(amount=5)
-    parser_vk.filter_content()
-    end = time.time()
-
-    parser_vk.preaty_print()
-
-    logger.info(f"Выполнилось за {end - start}")
+        print("Количество постов:", len(self.post_list))
